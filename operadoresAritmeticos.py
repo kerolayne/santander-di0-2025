5640ec2b6cf93532f7aa845a30b05d261ca7a8c1
kerolayne <kerolaynesouza@outlook.com.br>
2025-06-11 11:24:55 +0100

    Create operadoresAritmeticos.py

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,15 @@
+produto1 = 20
+produto2 = 30
+
+print(produto1 + produto2)  # Soma: 50
+print(produto1 - produto2)  # Subtração: -10
+print(produto1 * produto2)  # Multiplicação: 600
+print(produto1 / produto2)  # Divisão: 0.6666666666666666
+print(produto1 // produto2)  # Divisão inteira: 0
+print(produto1 % produto2)  # Módulo: 20
+print(produto1 ** produto2)  # Exponenciação: 1000000000000000000000000000000
+
+x = (10 + 5) * 2
+print(x)  # Resultado: 30
+y = 10 / 5 + 25 * 2 - 100 ** 2
+print(y)  # Resultado: -9999.0
