32313824191e9bb27bd1a13c99b4e20fb4c00eed
kerolayne <kerolaynesouza@outlook.com.br>
2025-06-11 11:29:19 +0100

    Create operadoresComparações.py

---
+++ operadoresComparações.py
@@ -0,0 +1,9 @@
+saldo = 1000
+saque = 500
+
+print(saldo >= saque)  # Verifica se o saldo é maior ou igual ao saque
+print(saldo > saque)   # Verifica se o saldo é maior que o saque
+print(saldo <= saque)  # Verifica se o saldo é menor ou igual ao saque
+print(saldo < saque)   # Verifica se o saldo é menor que o saque
+print(saldo == saque)  # Verifica se o saldo é igual ao saque
+print(saldo != saque)  # Verifica se o saldo é diferente do saque
