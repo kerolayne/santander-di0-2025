4a950cfef68f7a6a1c0365458397a8cc4e9bf8d3
kerolayne <kerolaynesouza@outlook.com.br>
2025-06-11 14:38:49 +0100

    Create operadores logicos.py

---
+++ operadores logicos.py
@@ -0,0 +1,17 @@
+saldo = 1000
+saque = 500
+limite = 2000
+conta_especial = True
+
+# Operadores lógicos
+print(True and True)  # True
+print(True and False)  # False
+print(False and False)  # False
+print(False or True)  # True
+print(True or True)  # True
+print(False or False)  # False
+
+expressao1 = saldo >= saque and saldo <= limite or conta_especial and saldo >= saque
+print(expressao1)  # Verifica se o saldo é suficiente para o saque considerando o limite e conta especial
+expressao2 = saldo >= saque and saldo <= limite or conta_especial and saldo >= saque
+print(expressao2)  # Verifica se o saldo é suficiente para o saque considerando o limite e conta especial
