c17858c4e5e3d5cc2c81507101ba0804d2c224b2
kerolayne <kerolaynesouza@outlook.com.br>
2025-06-24 14:40:20 +0100

    Update interfacesEclasseAbstrata.py

--- interfacesEclasseAbstrata.py
+++ interfacesEclasseAbstrata.py
@@ -15,3 +15,23 @@ print(p)
 
 p2 = Pessoas.criar_a_partir_data_nascimento(ano = 1990, mes = 5, dia = 15, nome = "Maria")
 print(p2.nome, p2.idade)
+
+
+## classe abstrata
+from abc import ABC, abstractmethod
+
+class ControleRemoto(ABC):
+    @abstractmethod
+    def ligar(self):
+        pass
+
+    @abstractmethod
+    def desligar(self):
+        pass
+
+class controleTV(ControleRemoto):
+    def ligar(self):
+        print("TV ligada")
+
+    def desligar(self):
+        print("TV desligada")
