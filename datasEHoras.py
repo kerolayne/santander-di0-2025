810bb0d55ec6f6d68d0334b5f94176c2e74f28cd
kerolayne <kerolaynesouza@outlook.com.br>
2025-06-24 14:34:19 +0100

    Update datasEHoras.py

--- datasEHoras.py
+++ datasEHoras.py
@@ -7,3 +7,19 @@ print(d.strftime("%d/%m/%Y %H:%M:%S"))
 d = d + datetime.timedelta(days=5, hours=2, minutes=30)
 print(d)
 print(d.strftime("%d/%m/%Y %H:%M:%S"))
+
+#trabalahndo com timezone
+import pytz
+
+# Definindo um fuso horário
+fuso_horario = pytz.timezone("America/Sao_Paulo")
+
+# Aplicando o fuso horário ao objeto datetime
+d = fuso_horario.localize(d)
+print(d)
+print(d.strftime("%d/%m/%Y %H:%M:%S"))
+
+# Convertendo para outro fuso horário
+d_utc = d.astimezone(pytz.utc)
+print(d_utc)
+print(d_utc.strftime("%d/%m/%Y %H:%M:%S"))
